90c609fb8731f2331609a94d6c4324f70e6449dc
maxgum21 <74920887+maxgum21@users.noreply.github.com>
2021-02-02 20:00:24 +0300

    короч я сделал рендер персонажа и карты а дату и спрайты щас скину в дискорд, протестируй п.с. Марио не может прыгать

--- main.py
+++ main.py
@@ -1,7 +1,150 @@
-def main():
-    print('Проверка, проверка. Раз, два, три!')
+import pygame
+
+size = (500, 500)
+sky_col = (200, 220, 255)
+running = True
+gravity = 1
+walkingX = False
+
+
+class Board:
+    def __init__(self, row, col):
+        self.mapList = []
+        self.texture_list = []
+        self.bList = []
+        self.char_loc = ()
+        self.row = row
+        self.col = col
+        self.charxy = (0, 0)
+        for i in range(self.col):
+            bListj = []
+            for j in range(self.row):
+                bListj.append(((j * 50, i * 50), ((j + 1) * 50, 50 * (i + 1)), '.'))  # x0,y0;x,y;block
+            self.bList.append(bListj)
+
+    def sort_map_text(self, fileName):
+        mapFile = [elem for num, elem in enumerate(open(fileName, 'r'))]
+        for i in range(self.row):
+            line = mapFile[i].rstrip('\n')
+            self.mapList.append(line)
+        maxlen = max(map(lambda x: len(x), self.mapList))
+        for elem in self.mapList:
+            elem.rjust(maxlen, '.')
+        map(lambda x: [x[i] for i in range(len(x))], self.mapList)
+
+        for i in range(self.row):
+            for j in range(self.col):
+                q = self.bList[i][j]
+                self.bList[i][j] = (q[0], q[1], self.mapList[i][j])
+
+    def render_map(self):
+        for i in range(self.row):
+            tl1 = []
+            for j in range(self.col):
+                elem = self.bList[i][j]
+                if elem[2] == '#':
+                    box = Grass()
+                    box.rect.x, box.rect.y = elem[0]
+                    tl1.append(box)
+                if elem[2] == '@':
+                    self.char_loc = (i, j)
+                    self.charxy = (self.bList[i][j][0][0] + 15, self.bList[i][j][0][1] + 5)
+
+            self.texture_list.append(tl1)
+        Ground_Sprites.draw(screen)
+
+
+class Grass(pygame.sprite.Sprite):
+    grass = pygame.image.load('Data/grass.png')
+
+    def __init__(self):
+        super().__init__(Ground_Sprites)
+        self.image = Grass.grass
+        self.rect = self.image.get_rect()
+
+
+class GroundChkr(pygame.sprite.Sprite):
+    gcheck = pygame.image.load('Data/groundChkr.png')
+
+    def __init__(self):
+        #  super().__init__(Char_Sprite)
+        self.image = GroundChkr.gcheck
+        self.rect = self.image.get_rect()
+
+
+class Mario(pygame.sprite.Sprite):
+    mario = pygame.image.load('Data/mario.png')
+
+    def __init__(self, board):
+        super().__init__(Char_Sprite)
+        self.image = Mario.mario
+        self.rect = self.image.get_rect()
+        self.xvelocity = 0
+        self.yvelocity = 0
+        self.grounded = False
+        self.board = board
+        self.gcheck = GroundChkr()
+        self.rect.x, self.rect.y = board.charxy
+
+    def updater(self):
+        Char_Sprite.draw(screen)
+        self.gcheck.rect.x = self.rect.x
+        self.gcheck.rect.y = self.rect.y + 47
+        if pygame.sprite.spritecollideany(self.gcheck, Ground_Sprites):
+            self.grounded = True
+
+        if self.grounded:
+            self.yvelocity = 0
+        else:
+            self.yvelocity -= gravity
+
+        if walkingX:
+            self.rect.x += self.xvelocity
+        self.rect.y -= self.yvelocity
+
+    def move(self, direction):
+        if direction == 'r':
+            self.xvelocity = 2
+        if direction == 'l':
+            self.xvelocity = -2
+
 
 if __name__ == '__main__':
-    main()
+    pygame.init()
+    pygame.display.set_caption('Марио в Коробках')
+    screen = pygame.display.set_mode(size)
+    screen.fill(sky_col)
 
-# Проверка Ппроверка  -  Максим
+    Ground_Sprites = pygame.sprite.Group()
+    Char_Sprite = pygame.sprite.Group()
+    UPDATER = pygame.USEREVENT + 1
+    pygame.time.set_timer(UPDATER, 10)
+    board = Board(10, 10)
+    board.sort_map_text('Data/map1.txt')
+    board.render_map()
+    mario = Mario(board)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    mario.jump()
+                if event.key == pygame.K_LEFT:
+                    walkingX = True
+                    mario.move('l')
+                if event.key == pygame.K_RIGHT:
+                    walkingX = True
+                    mario.move('r')
+            elif event.type == pygame.KEYUP:
+                walkingX = False
+            if event.type == UPDATER:
+                mario.updater()
+
+        screen.fill(sky_col)
+        Ground_Sprites.draw(screen)
+        Char_Sprite.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
